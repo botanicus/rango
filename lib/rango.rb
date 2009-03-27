db4da162bdcb2f7eed1c373fbaed33a462754cc1
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-03-27 23:41:28 +0100

    Written some basic doc in Yard style.

--- lib/rango.rb
+++ lib/rango.rb
@@ -7,6 +7,13 @@ require File.join(root, "core_ext")
 
 class Rango
   class << self
+    # === Examples:
+    # <pre>
+    # Rango.framework.root
+    # # => /usr/lib/ruby/lib/ruby/site_ruby/1.8/rango
+    # </pre>
+    #
+    # @return [OpenStruct] OpenStruct with informations about the framework. Options are: <code>root</code>.
     def framework
       root = File.join(File.dirname(__FILE__), "rango")
       framework = OpenStruct.new
@@ -14,18 +21,35 @@ class Rango
       return framework
     end
 
+    # === Examples:
+    # <pre>
+    # Rango.framework.root
+    # # => /usr/lib/ruby/lib/ruby/site_ruby/1.8/rango
+    # </pre>
+    #
+    # @return [Rango::Logger] Logger for logging framework-related stuff. For logging project-relating stuff please use Project.logger.
+    # @see Project::logger
     def logger
       Rango::Logger.new
     end
 
+    # === Examples:
+    # <pre>
     # Rango.import("router")
-    # Rango.import("router", :soft => true)
+    # Rango.import("orm/adapters/#{Project.orm}", :soft => true)
+    # </pre>
+    #
+    # @param [String] path Relative path from lib/rango
+    # @param [Hash] options Available options: soft
+    # @raise [LoadError] Unless soft importing is enable, it will raise LoadError if the file wasn't found
+    # @return [Boolean] Returns true if importing succeed or false if not.
     def import(path, options = Hash.new)
       if options[:soft]
         begin
           require File.join(Rango.framework.root, path)
         rescue LoadError
           Rango.logger.warn("File #{path} can't be loaded")
+          return false
         end
       else
         require File.join(Rango.framework.root, path)
@@ -37,15 +61,10 @@ end
 begin
   Rango.import("boot")
 rescue Exception => exception
-  # TODO: parse backtrace (another color for path, another for line and method)
-  # TODO: maybe relative paths for project files will be better
   Rango.logger.exception(exception)
 end
 
 # irb
 if ARGV.include?("-i")
   Rango.import("irb")
-else
-  # Rango.import("server")
-  # Rango::Server.start(4000)
 end
--- lib/rango/controller.rb
+++ lib/rango/controller.rb
@@ -1,6 +1,14 @@
 class Rango
   class Controller
-    attr_accessor :request, :params
+    # @return [Rango::Request] OpenStruct with informations about the framework. Options are: <code>root</code>.
+    # @see Rango::Request
+    attr_accessor :request
+    
+    # @return [Rango::Logger] Hash with params from request. For example <code>{:messages => {:success => "You're logged in"}, :post => {:id => 2}}</code>
+    attr_accessor :params
+
+    # @return [Rango::Logger] OpenStruct with informations about the framework. Options are: <code>root</code>.
+    # @see Rango::Logger
     attribute :logger, Project.logger
   end
 end
