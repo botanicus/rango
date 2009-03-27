315a6139168e8e07cd674904c549ae3d3b3a884d
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-03-27 13:27:01 +0100

    Improved strategies, it logs when strategy is registered, improved matching of correct strategy and fixed method run in ControllerStrategy.

--- lib/rango/router/strategies.rb
+++ lib/rango/router/strategies.rb
@@ -3,20 +3,24 @@ class Rango
     def register
       # TODO: different strategies for each router
       Rango::Router.strategies.push(self)
+      Rango.logger.debug("Strategy #{self} have been registered")
     end
   end
 
   # to("Post#show", "blog/views")
   # => Post.new(request).show(*args)
   class ControllerStrategy < RouterStrategy
-    def match?(route)
-      route.file && route.callable.is_a?(String)
+    def match?(request, params, *args, &block)
+      args.length.eql?(2) && args.all? { |arg| arg.is_a?(String) } && !block_given?
     end
 
-    def run(route, request, params)
-      Project.import(route.file) if route.file
+    def run(request, params, *args, &block)
+      file = args.first
+      callable = args.last
+      Project.import(file) if file.is_a?(String)
       args = params.map { |key, value| value }
-      klass, method = route.callable.split("#")
+      klass_name, method = callable.split("#")
+      klass = Object.const_get(klass_name)
       controller = klass.new
       controller.request = request
       controller.params  = params
@@ -27,13 +31,14 @@ class Rango
   # to(Rango.logger.method(:debug))
   # => Rango.logger.debug(request, *args)
   class CallableStrategy < RouterStrategy
-    def match?(route)
-      (not route.file) && route.callable.respond_to?(:call)
+    def match?(request, params, *args, &block)
+      (args.length.eql?(1) && args.first.respond_to?(:call)) || block_given?
     end
 
-    def run(route, request, params)
+    def run(request, params, *args, &block)
+      callable = args.first || block
       args = params.map { |key, value| value }
-      route.callable.call(request, *args)
+      callable.call(request, *args)
     end
   end
 end
