0533eb02f6ac5f9728a2db8a9af34dec3fc858cf
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-03-29 03:38:01 +0200

    Rango views helpers.

---
+++ lib/rango/helpers.rb
@@ -0,0 +1,21 @@
+class Rango
+  module Helpers
+    def textile(text)
+      require "redcloth"
+    end
+    
+    def markdown(text)
+      require "bluecloth"
+    end
+    
+    def maruku(text, options = Hash.new)
+      require "maruku"
+    end
+    
+    def syntax(text, options = Hash.new)
+      require "syntax/convertors/html"
+      convertor = Syntax::Convertors::HTML.for_syntax(options[:language] || "ruby")
+      convertor.convert(text, false)
+    end
+  end
+end
