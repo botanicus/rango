2152263ef3b05beb1c04d329cb5b0f81db719e80
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-03-29 03:38:38 +0200

    Created Rango::Dispatcher which is Rack application.

---
+++ lib/rango/dispatcher.rb
@@ -0,0 +1,21 @@
+Rango.import("handler")
+
+class Rango::Dispatcher < Rango::Handler
+  def call(env)
+    self.register
+    request  = Rango::Request.new(env)
+    route    = Project.router.find(request.path)
+    @body    = route.call(request)
+    response = super(env)
+    return [self.status, headers, self.body]
+  rescue Rango::HttpExceptions::HttpError => exception
+    return [exception.status, exception.headers, exception.body]
+  rescue Exception => exception
+    Project.logger.exception(exception)
+    @body = ["<h1>#{exception.message}</h1>"]
+    exception.backtrace.each do |trace|
+      @body.push("<li>#{trace}</li>")
+    end
+    return ["500", headers, self.body]
+  end
+end
