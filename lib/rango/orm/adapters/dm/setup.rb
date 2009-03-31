0edd13cb8ed10dc9d37204f0b5db1a1e27668d6d
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-03-31 13:25:21 +0200

    Fixed database connection.

--- lib/rango/orm/adapters/dm/setup.rb
+++ lib/rango/orm/adapters/dm/setup.rb
@@ -3,7 +3,7 @@
 begin
   adapter = Project.settings.database_adapter
   path = Project.settings.database_name
-  DataMapper.setup(:default, "#{adapter}::#{path}")
+  DataMapper.setup(:default, "#{adapter}://#{Project.root}/#{path}")
   Rango.logger.debug("DataMapper started with database #{path}")
 rescue
 end
