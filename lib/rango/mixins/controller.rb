a23e6f1da4eb739feb95710f4bdc807094f63f09
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-06-07 23:21:20 +0100

    Templates are imported in controller mixin, not anymore in controller itself.

--- lib/rango/mixins/controller.rb
+++ lib/rango/mixins/controller.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+Rango.import("templates/template")
+
 # This mixin should be included to the all objects which are supposed to return response for Rack, so not just ControllerStrategy, but also CallableStrategy
 class Rango
   module ControllerMixin
