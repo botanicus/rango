1687f1207dd8fbded5062fd8056c34e4d6e62d27
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-06-07 23:19:04 +0100

    The app method and similar stuff will be part of Rango::Mini mixin, you will need to include it to the Rack::Builder yourself.

---
+++ lib/rango/mixins/mini.rb
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+Rango.import("mixins/controller")
+
+# run app { |request, response|
+# if request.env["PATH_INFO"].match(/^\/admin/)
+#   redirect ""
+# else
+#   render "index"
+# end
+# }
+class Rango
+  module Mini
+    include Rango::ControllerMixin
+    def app(&block)
+      lambda do |env|
+        request = Rango::Request.new(env)
+        response = Rack::Response.new
+        body = block.call(request, response)
+        # TODO: check how rack test if object is stringable, probably not this way
+        raise ArgumentError unless body.respond_to?(:each) || body.is_a?(String)
+        response.write(body)
+        response.finish
+      end
+    end
+  end
+end
