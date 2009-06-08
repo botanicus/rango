48139265b9b112c37914a8b5e185113ab0bc1bfe
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-06-08 22:43:40 +0100

    Controller renamed

--- lib/rango/generic_views.rb
+++ lib/rango/generic_views.rb
@@ -2,31 +2,17 @@
 
 Rango.import("mixins/mini")
 
-# http://gist.github.com/125500
 class Rango
   module GenericViews
     include Rango::Mini
-    extend  Rango::Mini
-
-    @@generic ||= Array.new
-    # GenericViews.add(:hello) do |request, reponse|
-    #   [200, Hash.new, ["Hello Generic Views!"]]
-    # end
-    def self.add(name, &block)
-      @@generic.push(name)
-      define_method(name, &block)
-      self.metaclass.send(:define_method, :"__#{name}__", &block)
-      self.metaclass.send(:define_method, name) do
-        app do |*args|
-          self.send(:"__#{name}__", *args)
-        end
+    def static
+      app do |request, response|
+        template = "#{request.script_name[1..-1]}.html"
+        Rango.logger.debug("Rendering '#{template}'")
+        render template
       end
     end
   end
 end
 
-Rango::GenericViews.add(:static) do |request, response|
-  template = "#{request.script_name[1..-1]}.html"
-  Rango.logger.debug("Rendering '#{template}'")
-  render template
-end
+Rack::Builder.send(:include, Rango::GenericViews)
--- lib/rango/mvc/strategies.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-class Rango
-  module RenderMixin
-  end
-end
