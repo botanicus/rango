a108a01fcaed0ff6a9e9212fb5959c8886eeaa92
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-06-10 16:03:32 +0100

    All middlewares moved to the Rack::Middlewares module (issue #25)

--- lib/rango/rack/middlewares/email_obfuscator.rb
+++ lib/rango/rack/middlewares/email_obfuscator.rb
@@ -2,22 +2,24 @@
 
 # TODO: it can be in rack-contrib
 class Rango
-  class EmailObfuscator
-    def initialize(app)
-      @app = app
-    end
-
-    def call(env)
-      status, headers, body = @app.call(env)
-      if headers["Content-Type"] == "text/html"
-        body = self.substitute(body)
+  module Middlewares
+    class EmailObfuscator
+      def initialize(app)
+        @app = app
       end
-      [status, headers, body]
-    end
 
-    # TODO: is it OK everywhere? E. g. <script> or <style>
-    def substitute(body)
-      body.map { |chunk| chunk.gsub!("@", "&#x40;") }
+      def call(env)
+        status, headers, body = @app.call(env)
+        if headers["Content-Type"] == "text/html"
+          body = self.substitute(body)
+        end
+        [status, headers, body]
+      end
+
+      # TODO: is it OK everywhere? E. g. <script> or <style>
+      def substitute(body)
+        body.map { |chunk| chunk.gsub!("@", "&#x40;") }
+      end
     end
   end
 end
--- lib/rango/rack/middlewares/static.rb
+++ lib/rango/rack/middlewares/static.rb
@@ -4,48 +4,50 @@
 require 'rack/utils'
 
 class Rango
-  class Static
-    FILE_METHODS = %w(GET HEAD).freeze
+  module Middlewares
+    class Static
+      FILE_METHODS = %w(GET HEAD).freeze
 
-    # @since 0.0.2
-    def initialize(app)
-      @app = app
-      @file_server = ::Rack::File.new(Project.settings.media_root)
-    end
-
-    # @since 0.0.2
-    def call(env)
-      path        = env['PATH_INFO'].chomp('/')
-      method      = env['REQUEST_METHOD']
-
-      prefix = Project.settings.media_prefix.chomp("/")
-      prefix_regexp = Regexp.new(%r[^#{prefix}/])
-      if path.match(prefix_regexp) && FILE_METHODS.include?(method)
-        if file_exist?(path)
-          return @file_server.call(env)
-          # else
-          # cached_path = directory_exist?(path) ? "#{path}/index" : path
-          # cached_path += ::ActionController::Base.page_cache_extension
-          #
-          # if file_exist?(cached_path)
-          #   env['PATH_INFO'] = cached_path
-          #   return @file_server.call(env)
-          # end
-        end
+      # @since 0.0.2
+      def initialize(app)
+        @app = app
+        @file_server = ::Rack::File.new(Project.settings.media_root)
       end
 
-      @app.call(env)
-    end
+      # @since 0.0.2
+      def call(env)
+        path        = env['PATH_INFO'].chomp('/')
+        method      = env['REQUEST_METHOD']
 
-    private
-    def file_exist?(path)
-      full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
-      File.file?(full_path) && File.readable?(full_path)
-    end
+        prefix = Project.settings.media_prefix.chomp("/")
+        prefix_regexp = Regexp.new(%r[^#{prefix}/])
+        if path.match(prefix_regexp) && FILE_METHODS.include?(method)
+          if file_exist?(path)
+            return @file_server.call(env)
+            # else
+            # cached_path = directory_exist?(path) ? "#{path}/index" : path
+            # cached_path += ::ActionController::Base.page_cache_extension
+            #
+            # if file_exist?(cached_path)
+            #   env['PATH_INFO'] = cached_path
+            #   return @file_server.call(env)
+            # end
+          end
+        end
 
-    def directory_exist?(path)
-      full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
-      Dir.exist?(full_path) && File.readable?(full_path)
+        @app.call(env)
+      end
+
+      private
+      def file_exist?(path)
+        full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
+        File.file?(full_path) && File.readable?(full_path)
+      end
+
+      def directory_exist?(path)
+        full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
+        Dir.exist?(full_path) && File.readable?(full_path)
+      end
     end
   end
 end
