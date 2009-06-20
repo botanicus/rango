5783fbad18e644c5662881a0d575b3237f6c85dc
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-06-20 22:12:15 +0100

    Updated Haml adapter

--- lib/rango/templates/adapters/haml.rb
+++ lib/rango/templates/adapters/haml.rb
@@ -1,16 +1,25 @@
 # encoding: utf-8
 
+# Features:
+#   - autoescaping
+#   - caching (not yet)
+
 require "haml"
 Rango.import("templates/adapter")
 
 module Rango
   module Templates
-    class Haml < ::Rango::Templates::Adapter
+    class HamlAdapter < Adapter
       # @since 0.0.2
       def render(io, scope, locals = Hash.new)
         options = Project.settings.haml.to_hash ### FIXME
         options[:filename] = io.path
-        ::Haml::Engine.new(io.read, options).render(scope, locals)
+        if File.exist?("#{io.path}.cache")
+          eval(File.read("#{io.path}.cache"), scope)
+        else
+          engine = Haml::Engine.new(io.read, options)
+          engine.render(scope, locals)
+        end
       end
     end
   end
