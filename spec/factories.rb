380ba030a82e3e31b95ebdc1c2ad49a65e5f102b
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-07-18 17:30:23 +0100

    Updated specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 
+require "rack/mock"
+
 class Factory
   class << self
     def rack_env(environment = Hash.new)
       {"PATH_INFO" => "/blog/post/a-slug", "REQUEST_METHOD" => "GET"}.merge(environment)
     end
 
-    # TODO: Rack MocRequest
     def request
       Rango::Request.new(self.rack_env)
     end
