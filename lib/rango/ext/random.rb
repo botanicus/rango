8d6642be5375b000cfa94c4d06e55905e16baffd
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-07-22 18:54:00 +0100

    Updated doc, removed Hash#rand

--- lib/rango/ext/random.rb
+++ lib/rango/ext/random.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 
 class Array
+  # @author Botanicus
+  # @since 0.0.2
+  # @return [Object] Random item from array
   def rand
     self[Kernel.rand(length)]
   end
@@ -8,15 +11,11 @@ class Array
 end
 
 class Range
+  # @author Botanicus
+  # @since 0.0.2
+  # @return [Object] Random item from range
   def rand
     self.min + Kernel.rand(self.max)
-  end
-  alias_method :random, :rand
-end
-
-class Hash
-  def rand
-    self[self.keys.random]
   end
   alias_method :random, :rand
 end
