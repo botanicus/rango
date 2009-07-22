01c46f66047d5631d8af6725e299f4825d607ced
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-07-22 18:53:41 +0100

    Updated doc

--- lib/rango/ext/object_space.rb
+++ lib/rango/ext/object_space.rb
@@ -5,28 +5,28 @@
 # TODO: isn't it already in extlib?
 
 module ObjectSpace
-  # @return [Array<Class>] List of all classes loaded into runtime
   # @author Botanicus
   # @since 0.0.3
+  # @return [Array<Class>] List of all classes loaded into runtime
   def self.classes
     self.all(Class)
   end
 
-  # @return [Array<Module>] List of all modules loaded into runtime
   # @author Botanicus
   # @since 0.0.3
+  # @return [Array<Module>] List of all modules loaded into runtime
   def self.modules
     self.all(Module)
   end
 
+  # @author Botanicus
+  # @since 0.0.3
   # @param [Class, Module]
   # @example Get all numbers
   #   ObjectSpace.all(Numeric) # => [1, 0.3, ...]
   # @example Get all numbers
   #   ObjectSpace.all(Numeric) # => [1, 0.3, ...]
   # @return [Array] List of all objects of given class or list of all objects
-  # @author Botanicus
-  # @since 0.0.3
   def self.all(klass)
     Array.new.tap do |objects|
       self.each_object(klass) do |object|
