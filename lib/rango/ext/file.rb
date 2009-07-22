a474bd01d9efec747d534c92be8a9b6fa50ec624
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-07-22 17:06:04 +0100

    Added File.append and File.add

--- lib/rango/ext/file.rb
+++ lib/rango/ext/file.rb
@@ -1,18 +1,28 @@
 class File
+  # File.append "~/.bashrc", source
+  def self.append(file, *chunks)
+    self.write(:puts, "a", file, *chunks)
+  end
+
+  # File.append "~/.bashrc", source
+  def self.add(file, *chunks)
+    self.write(:print, "a", file, *chunks)
+  end
+
   # File.puts "~/.bashrc", source
   def self.puts(file, *chunks)
-    self.write(:puts, file, *chunks)
+    self.write(:puts, "w", file, *chunks)
   end
 
   # File.print "~/.bashrc", source
   def self.print(file, *chunks)
-    self.write(:print, file, *chunks)
+    self.write(:print, "w", file, *chunks)
   end
 
   # File.write :printf, "~/.bashrc", "%d %04x", 123, 123
-  def self.write(method, file, *args)
+  def self.write(method, mode, file, *args)
     self.expand_path(file).tap do |path|
-      self.open(path, "w") { |file| file.send(method, *args) }
+      self.open(path, mode) { |file| file.send(method, *args) }
     end
   end
 end
--- spec/rango/ext/file_spec.rb
+++ spec/rango/ext/file_spec.rb
@@ -22,30 +22,56 @@ describe File do
   end
 
   describe ".print" do
-    it "should write all arguments without any whitespaces" do
+    it "should write all arguments without any extra whitespaces" do
       path = File.print(@path, "first", "second")
+      File.read(path).should eql("firstsecond")
+    end
+  end
+
+  describe ".append" do
+    it "should works like File.puts for new files" do
+      path = File.append(@path, "first", "second")
+      File.read(path).should eql("first\nsecond\n")
+    end
+
+    it "should append all arguments separated by \\n" do
+      File.puts(@path, "first")
+      path = File.append(@path, "second")
+      File.read(path).should eql("first\nsecond\n")
+    end
+  end
+
+  describe ".add" do
+    it "should works like File.print for new files" do
+      path = File.add(@path, "first", "second")
+      File.read(path).should eql("firstsecond")
+    end
+
+    it "should append all arguments without any extra whitespaces" do
+      File.print(@path, "first")
+      path = File.add(@path, "second")
       File.read(path).should eql("firstsecond")
     end
   end
 
   describe ".write" do
     it "should expand file path" do
-      File.write(:puts, @path, "content")
+      File.write(:puts, "w", @path, "content")
       File.exist?(File.expand_path(@path)).should be_true
     end
 
     it "should returns expanded file path" do
-      File.write(:puts, @path, "test").should eql(File.expand_path(@path))
+      File.write(:puts, "w", @path, "test").should eql(File.expand_path(@path))
     end
 
     it "should use first argument as method sended to file" do
       first  = File.puts("#{@path}.rw", "first", "second")
-      second = File.write(:puts, @path, "first", "second")
+      second = File.write(:puts, "w", @path, "first", "second")
       File.read(first).should eql(File.read(second))
     end
 
     it "should use 3..n arguments as arguments of given method" do
-      path = File.write(:printf, @path, "%d %04x", 123, 123)
+      path = File.write(:printf, "w", @path, "%d %04x", 123, 123)
       File.read(path).should eql("123 007b")
     end
   end
