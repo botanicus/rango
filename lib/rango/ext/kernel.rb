8466f013247e55bf866bd2dc968ebb851f9bc208
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-07-22 18:24:36 +0100

    Object#try moved to Kernel#try

--- lib/rango/ext/kernel.rb
+++ lib/rango/ext/kernel.rb
@@ -87,4 +87,31 @@ module Kernel
     p(*args)
     return *args
   end
+
+  # stolen from rails (but our definition isn't compatible)
+  # rails defines try as __send__ for each objects exclude nil - nil#try returns nil
+  # our try returns nil if the method doesn't exist
+  #
+  # Invokes the method identified by the symbol +method+, passing it any arguments
+  # and/or the block specified, just like the regular Ruby <tt>Object#send</tt> does.
+  #
+  # *Unlike* that method however, a +NoMethodError+ exception will *not* be raised
+  # and +nil+ will be returned instead, if the receiving object is a +nil+ object or NilClass.
+  #
+  # @example
+  #
+  # Without try
+  #   @person && @person.name
+  # or
+  #   @person ? @person.name : nil
+  #
+  # With try
+  #   @person.try(:name)
+  #
+  # +try+ also accepts arguments and/or a block, for the method it is trying
+  #   Person.try(:find, 1)
+  #   @people.try(:collect) {|p| p.name}
+  def try(method, *args, &block)
+    self.send(method, *args, &block) if self.respond_to?(method)
+  end
 end
