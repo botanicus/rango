e4792173fdc1938c35f07d3c8efb94903dcd0afa
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-07-23 11:52:43 +0100

    Kernel#acquire and Kernel#acquire_relative

--- lib/rango/ext.rb
+++ lib/rango/ext.rb
@@ -1,11 +1,33 @@
 # encoding: utf-8
 
-require_relative "ext/class"
-require_relative "ext/attribute"
-require_relative "ext/kernel"
-require_relative "ext/hash"
-require_relative "ext/string"
-require_relative "ext/colored_string"
-require_relative "ext/random"
-require_relative "ext/object_space"
-require_relative "ext/time_dsl"
+module Kernel
+  def acquire(glob, params = Hash.new)
+    path = $:.find { |path| File.directory(path) }
+    __acquire__(path, glob, params)
+  end
+
+  def acquire_relative(glob, params = Hash.new)
+    path = File.dirname(caller[0].split(":").first)
+    __acquire__(path, glob, params)
+  end
+
+  private
+  def __acquire__(path, glob, params)
+    glob.replace("#{glob}.rb") if File.split(path).last.eql?("*")
+    files = Dir[File.join(path, glob)]
+    return if files.empty?
+    excludes = [params[:exclude]].flatten
+    excludes.map! do |glob|
+      path = File.join(path, glob)
+      File.file?(path) ? path : Dir[path]
+    end
+    excludes = excludes.flatten.compact
+    files.select do |path|
+      if File.file?(path) && !excludes.include?(path)
+        require path
+      end
+    end
+  end
+end
+
+acquire_relative "ext/*", exclude: "ext/thor.rb"
