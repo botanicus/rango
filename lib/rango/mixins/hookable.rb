3dfc4c1483bb3c90a5f3f13651c2a03a19f8ded3
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-07-25 17:25:26 +0100

    Hookable & Chainable moved into mixins

---
+++ lib/rango/mixins/hookable.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+# class Test
+#   extend Rango::Hookable
+#   install_hook do |instance|
+#     p instance
+#   end
+#
+#   def initialize
+#     p self
+#   end
+# end
+#
+# Test.new
+
+module Rango
+  module Hookable
+    def new(*args)
+      instance = super(*args)
+      self.hooks.each { |hook| hook.call(instance) }
+      return instance
+    end
+
+    def hooks
+      @hooks ||= Array.new
+    end
+
+    def install_hook(&block)
+      self.hooks.push(block)
+    end
+  end
+end
