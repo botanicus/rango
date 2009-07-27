892826b6b7acff8bc5353e32653fe4d285d72ab1
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-07-27 21:28:53 +0100

    Brackets instead of apostroves

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,29 +1,29 @@
 # encoding: utf-8
 
-require 'tempfile'
-require 'spec/expectations'
-require 'fileutils'
-require 'forwardable'
+require "tempfile"
+require "spec/expectations"
+require "fileutils"
+require "forwardable"
 
 class CucumberWorld
   extend Forwardable
   def_delegators CucumberWorld, :examples_dir, :self_test_dir, :working_dir, :cucumber_lib_dir
 
   def self.examples_dir(subdir=nil)
-    @examples_dir ||= File.expand_path(File.join(File.dirname(__FILE__), '../../examples'))
+    @examples_dir ||= File.expand_path(File.join(File.dirname(__FILE__), "../../examples"))
     subdir ? File.join(@examples_dir, subdir) : @examples_dir
   end
 
   def self.self_test_dir
-    @self_test_dir ||= examples_dir('self_test')
+    @self_test_dir ||= examples_dir("self_test")
   end
 
   def self.working_dir
-    @working_dir ||= examples_dir('self_test/tmp')
+    @working_dir ||= examples_dir("self_test/tmp")
   end
 
   def cucumber_lib_dir
-    @cucumber_lib_dir ||= File.expand_path(File.join(File.dirname(__FILE__), '../../lib'))
+    @cucumber_lib_dir ||= File.expand_path(File.join(File.dirname(__FILE__), "../../lib"))
   end
 
   def initialize
@@ -36,7 +36,7 @@ class CucumberWorld
   def create_file(file_name, file_content)
     file_content.gsub!("CUCUMBER_LIB", "'#{cucumber_lib_dir}'") # Some files, such as Rakefiles need to use the lib dir
     in_current_dir do
-      File.open(file_name, 'w') { |f| f << file_content }
+      File.open(file_name, "w") { |f| f << file_content }
     end
   end
 
@@ -45,7 +45,7 @@ class CucumberWorld
   end
 
   def run(command)
-    stderr_file = Tempfile.new('cucumber')
+    stderr_file = Tempfile.new("cucumber")
     stderr_file.close
     in_current_dir do
       @last_stdout = `#{command} 2> #{stderr_file.path}`
