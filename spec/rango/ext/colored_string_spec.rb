6c9be801ac9a12d26fa73858a453ecc39e8f923a
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-07-28 09:39:33 +0100

    Skitch of Kernel and ColoredString spec files

---
+++ spec/rango/ext/colored_string_spec.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require_relative "../../../lib/rango/ext/colored_string"
+
+describe ColoredString do
+  describe "#TODO" do
+    it
+  end
+end
---
+++ spec/rango/ext/kernel_spec.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require_relative "../../../lib/rango/ext/kernel"
+
+describe Kernel do
+  describe "#TODO" do
+    it
+  end
+end
