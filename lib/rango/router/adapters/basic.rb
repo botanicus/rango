d200ab0eb621fe750bb248684351b4b0a2a27268
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-09-05 10:00:08 +0100

    All the router adapters are using Router.implement now

--- lib/rango/router/adapters/basic.rb
+++ lib/rango/router/adapters/basic.rb
@@ -6,16 +6,7 @@
 
 Rango.import("router/dispatcher")
 
-module Rango
-  class Router
-    module Basic
-      def set_rack_env(env)
-        env["rango.router.params"] = Hash.new
-        env["rango.router.app"] = self
-      end
-
-      # register it
-      Rango::Router::Dispatcher.router_adapter = self
-    end
-  end
+Rango::Router.implement(:rack_router) do |env|
+  env["rango.router.params"] = Hash.new
+  env["rango.router.app"] = self
 end
--- lib/rango/router/adapters/rack-router.rb
+++ lib/rango/router/adapters/rack-router.rb
@@ -3,16 +3,7 @@
 require "rack/router"
 Rango.import("router/dispatcher")
 
-module Rango
-  class Router
-    module RackRouter
-      def set_rack_env(env)
-        env["rango.router.params"] = env["rack_router.params"]
-        env["rango.router.app"] = self
-      end
-
-      # register it
-      Rango::Router::Dispatcher.router_adapter = self
-    end
-  end
+Rango::Router.implement(:rack_router) do |env|
+  env["rango.router.params"] = env["rack_router.params"]
+  env["rango.router.app"] = self
 end
