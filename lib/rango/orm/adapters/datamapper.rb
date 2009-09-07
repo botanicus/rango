10be60852a5fad3aaad3766dae4c2e33fd8e3fe0
Jakub Šťastný aka Botanicus <jakub.stastny@jgp.co.uk>
2009-09-07 15:59:51 +0100

    Removed extra code

--- lib/rango/orm/adapters/datamapper.rb
+++ lib/rango/orm/adapters/datamapper.rb
@@ -16,20 +16,3 @@ module Rango
     end
   end
 end
-
-module Rango
-  module ORM
-    class Sequel
-      def load
-        Rango.dependency "sequel", gem: true
-        require_relative "support"
-      end
-
-      def connect
-        try_connect do |adapter, path|
-          DataMapper.setup(:default, "#{adapter}://#{Project.root}/#{path}")
-        end
-      end
-    end
-  end
-end
