a06e5acc530a56c59a254b6dce228f491510388d
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-09-17 21:26:11 +0100

    Rango::Controller#url as shortcut for working with named routes when using Usher router

--- lib/rango/router/adapters/usher.rb
+++ lib/rango/router/adapters/usher.rb
@@ -6,3 +6,12 @@ Rango::Router.implement(:usher) do |env|
   env["rango.router.params"] = env["usher.params"]
   env["rango.router.app"] = self
 end
+
+module Rango
+  class Controller
+    # url(:login)
+    def url(*args)
+      Project.router.router.generator.generate(*args)
+    end
+  end
+end
