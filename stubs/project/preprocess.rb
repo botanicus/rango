27c529e48ba468582991ffbe5a3cd903ed7702cc
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-10-27 08:29:35 +0000

    Skitch of preprocess hook for project generator

---
+++ stubs/project/preprocess.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+# This hook will run before templater creates new files from templates
+# You should setup your variables for templates here
+# Dir.pwd => the stubs directory location
+
+# rango create app blog post,tag posts,tags
+content_dir = ARGV.shift
+application = ARGV.shift
+models = ARGV.shift.try(:split, ",") || Array.new
+controllers = ARGV.shift.try(:split, ",") || Array.new
+
+context = {application: application, models: models, controllers: controllers}
+Rango::CLI::Templater.create(content_dir, context)
