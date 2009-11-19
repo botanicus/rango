9c6a85f520af621c6ad53fb5a933c544704b65a7
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-19 10:27:09 +0000

    preprocess.rb changed to setup.rb

---
+++ stubs/app/setup.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+# This hook will run before simple-templater creates new files from templates
+# You can update context hash. Don't forget to use merge! instead of merge,
+# because you are manipulating with one object, rather than returning new one
+# Dir.pwd => empty directory where the project will be located
+
+# rango create app blog --models=post,tag --controllers=posts,tags
+setup do |generator, context|
+  models  = context[:models] || Array.new
+  controllers = context[:controllers] || Array.new
+  context.merge!(models: models, controllers: controllers)
+end
---
+++ stubs/flat/setup.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+# This hook will run before simple-templater creates new files from templates
+# You can update context hash. Don't forget to use merge! instead of merge,
+# because you are manipulating with one object, rather than returning new one
+# Dir.pwd => empty directory where the project will be located
+
+setup do |generator, context|
+  #
+end
