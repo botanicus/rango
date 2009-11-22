1b72f0f983068c0a4a6fe801bc32d048e7629f25
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-22 21:08:24 +0000

    Support for multiple template engines

--- stubs/project/setup.rb
+++ stubs/project/setup.rb
@@ -4,10 +4,11 @@
 # You can update context hash and register hooks. Don't forget to use merge! instead of merge, because you are
 # manipulating with one object, rather than returning new one.
 
-# rango create app blog --models=post,tag --controllers=posts,tags --router=usher|rack-router|rack-mount
+# rango create app blog --models=post,tag --controllers=posts,tags --router=usher|rack-router|rack-mount --template-engine=erubis
 hook do |generator, context|
   models  = context[:models] || Array.new
   controllers = context[:controllers] || Array.new
   context.merge!(models: models, controllers: controllers)
   context[:router] = "usher" unless context[:router]
+  context[:template_engine] = "haml" unless context[:template_engine]
 end
