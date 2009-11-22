a7eb045fd2ccf1239204f659838bb6854a406497
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-22 21:50:38 +0000

    Include UrlHelper into RenderMixin

--- lib/rango/mixins/render.rb
+++ lib/rango/mixins/render.rb
@@ -5,6 +5,8 @@ require "rango/templates/template"
 # This mixin should be included to the all objects which are supposed to return response for Rack, so not just ControllerStrategy, but also CallableStrategy
 module Rango
   module RenderMixin
+    include Rango::UrlHelper
+
     # @since 0.0.1
     # @return [Rango::Request]
     # @see Rango::Request
