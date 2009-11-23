6f83437288c397859fc33a4f4798abd9df443dd9
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-23 17:49:53 +0000

    Project generator done

--- stubs/project/postprocess.rb
+++ stubs/project/postprocess.rb
@@ -7,8 +7,8 @@ require "rubyexts/platform"
 require "simple-templater/hooks/postprocess/git_repository"
 
 if RubyExts::Platform.unix?
-  sh "chmod +x boot.rb"
 	sh "chmod +x init.rb"
+  sh "chmod +x config.ru"
 end
 
 hook do |generator, context|
