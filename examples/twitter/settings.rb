4e15e30557873e15ffd9a87626aa29bdcc7ff43f
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-24 10:25:41 +0000

    Removed examples

--- examples/twitter/settings.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-Project.configure do
-  self.media_root = "media"
-end
--- examples/twitter/settings_local.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-# these settings will be merged with settings in main settings file
-Project.configure do
-  self.database_adapter = "sqlite3"
-end
