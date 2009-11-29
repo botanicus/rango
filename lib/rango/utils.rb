95e5faa704494d7b36b17a03b40b167f9f56227a
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-29 19:52:49 +0000

    Rango::Utils can works as a mixin

--- lib/rango/utils.rb
+++ lib/rango/utils.rb
@@ -2,9 +2,16 @@
 
 require "rack"
 
+# class App
+#   extend Rango::Utils
+#   def load_rackup
+#     super(File.join(File.join(self.root, "config.ru")), "config.ru")
+#   end
+# end
 module Rango
   module Utils
-    def self.load_rackup(path = "config.ru")
+    extend self
+    def load_rackup(path = "config.ru")
       eval("Rack::Builder.new {( #{File.read(path)}\n )}.to_app", nil, File.expand_path(path))
     end
   end
--- spec/rango/utils_spec.rb
+++ spec/rango/utils_spec.rb
@@ -7,6 +7,10 @@ require "rack/mock"
 require "rango/utils"
 
 describe Rango::Utils do
+  it "should work as a mixin, so we can include it into our app mixin and redefine it" do
+    Class.new { extend Rango::Utils }.should respond_to(:load_rackup)
+  end
+
   describe ".load_rackup" do
     it "should take path to rackup as the first argument" do
       rackup = File.join("spec", "stubs", "config.ru")
