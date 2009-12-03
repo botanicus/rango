e5367c11e95ab3a6573b7bf6b8c1d29ef64e00d8
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-03 16:55:01 +0000

    Removed a note

--- lib/rango/gv/static.rb
+++ lib/rango/gv/static.rb
@@ -1,9 +1,5 @@
 # encoding: utf-8
 
-# Generic Views
-# Use directly or include into a controller if you want to use filters or customize them
-# http://wiki.github.com/botanicus/rango/generic-views
-
 require "rango/gv"
 
 module Rango
