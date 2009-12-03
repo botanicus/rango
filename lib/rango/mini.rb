ae9bc1374bce47855e0cc1719bb4db74db95fdad
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-03 16:44:33 +0000

    More useful error message

--- lib/rango/mini.rb
+++ lib/rango/mini.rb
@@ -14,11 +14,12 @@ module Rango
     def app(&block)
       raise ArgumentError, "Block is required" unless block_given?
       lambda do |env|
+        Rango::Router.set_rack_env(env)
         request = Rango::Request.new(env)
         response = Rack::Response.new
         body = block.call(request, response)
         # TODO: check how rack test if object is stringable, probably not this way
-        raise ArgumentError unless body.respond_to?(:each) || body.is_a?(String)
+        raise ArgumentError, "It has to return a valid rack body, #{body.inspect} returned" unless body.respond_to?(:each) || body.is_a?(String)
         response.write(body)
         array = response.finish
         [array[0], array[1], body] # we don't want to have Rack::Response instance instead body, it's mess!
