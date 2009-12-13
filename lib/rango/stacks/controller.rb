ae6f2903379aa201101b4a72e878cc3e8cf10a10
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-13 13:14:46 +0000

    Stack files for more DRY initialization

---
+++ lib/rango/stacks/controller.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+require "rango"
+require "rango/controller"
+require "rango/environments"
+require "rango/rack/middlewares/basic"
---
+++ lib/rango/stacks/mini.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+require "rango"
+require "rango/mini"
+require "rango/rack/middlewares/basic"
