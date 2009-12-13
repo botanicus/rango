de784d834244c02ec0bad6bd1e2a9bb09cebd581
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-13 18:15:50 +0000

    More goodies into controller stack

--- lib/rango/stacks/controller.rb
+++ lib/rango/stacks/controller.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 require "rango"
+require "rango/logger"
+require "rango/interactive"
 require "rango/controller"
 require "rango/environments"
 require "rango/rack/middlewares/basic"
