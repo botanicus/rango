b65dd1a770443c247837020ea2bf2181d793105d
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-03 17:53:16 +0100

    Added git-deployer & code-cleaner integration

--- stubs/stack/setup.rb
+++ stubs/stack/setup.rb
@@ -4,7 +4,7 @@
 # You can update context hash and register hooks. Don't forget to use merge! instead of merge, because you are
 # manipulating with one object, rather than returning new one.
 
-# rango create app blog --models=post,tag --controllers=posts,tags --router=usher|rack-router|rack-mount --template-engine=erubis
+# rango create app blog --models=post,tag --controllers=posts,tags --router=usher|rack-router|rack-mount --template-engine=erubis --no-code-cleaner --no-git-deployer
 hook do |generator, context|
   models = [context[:models]].compact.flatten # the flatten thing: if you have --models=post, it would be just a string
   controllers = [context[:controllers]].compact.flatten
@@ -12,4 +12,6 @@ hook do |generator, context|
   context[:orm] = "datamapper" unless context[:orm]
   context[:router] = "usher" unless context[:router]
   context[:template_engine] = "haml" unless context[:template_engine]
+  context[:git_deployer] = true unless context.has_key?(:git_deployer)
+  context[:code_cleaner] = true unless context.has_key?(:git_deployer)
 end
