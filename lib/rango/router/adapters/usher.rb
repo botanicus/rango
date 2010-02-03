cd138ae499a6c8605f58f046d039c9e5e0e41b82
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-02-03 22:00:49 +0000

    Fixed params when 404 occures in usher

--- lib/rango/router/adapters/usher.rb
+++ lib/rango/router/adapters/usher.rb
@@ -3,7 +3,8 @@
 require "usher"
 
 Rango::Router.implement(:usher) do |env|
-  env["rango.router.params"] = env["usher.params"]
+  # when usher routes to the default app, then usher.params is nil
+  env["rango.router.params"] = env["usher.params"] || Hash.new
 end
 
 module Rango
