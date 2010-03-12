a0e20deda7320344befb0a25dbbb126b7f6b9686
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-12 21:32:12 +0000

    Less monkey-patching

--- lib/rango/templates/exts/haml.rb
+++ lib/rango/templates/exts/haml.rb
@@ -8,23 +8,14 @@
 # `text/javascript`. For example, `%script` compiles to `<script type="text/javascript"></script>`.
 # Defaults to `{script: {type: "text/javascript"}, form: {method: "POST"}}`
 
-module Haml
-  class Engine
-    alias_method :__initialize__, :initialize
-    def initialize(template, options = Hash.new)
-      __initialize__(template, options)
-      @options[:default_attributes] ||= Hash.new
-      @options[:default_attributes] = @options[:default_attributes].inject(Hash.new) do |options, pair|
-        options.merge(pair.first.to_sym => pair.last)
-      end
-    end
-  end
+Tilt::HamlTemplate.options ||= Hash.new
 
+module Haml
   module Precompiler
     alias_method :__prerender_tag__, :prerender_tag
     def prerender_tag(name, self_close, attributes)
       # merge given attributes with default attributes from options
-      if default_attributes = @options[:default_attributes][name.to_sym]
+      if default_attributes = Tilt::HamlTemplate.options[:default_attributes]
         attributes = default_attributes.merge(attributes)
       end
       __prerender_tag__(name, self_close, attributes)
