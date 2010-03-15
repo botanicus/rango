b15064533897434fe240afd3c322fdc80ba5fdbf
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-15 00:56:57 +0000

    Fixed issue in Sequel pagination adapter

--- lib/rango/contrib/pagination/adapters/sequel.rb
+++ lib/rango/contrib/pagination/adapters/sequel.rb
@@ -5,10 +5,14 @@ module Rango
     module Sequel
       module Dataset
         # @since 0.2.2
-        def paginate(current = 1, per_page = Page.per_page, options = Hash.new)
+        # @example
+        #   Sticker.paginate(page, 20, online: false)
+        #   Sticker.filter(online: false).paginate(page)
+        def paginate(current = 1, per_page = Page.per_page, options = nil)
           current = current ? current.to_i : 1 # current can be nil or "1"
           page = Page.new(current: current, count: self.count, per_page: per_page)
-          return page, limit(per_page, (current - 1) * per_page)
+          dataset = options ? self.filter(options) : self
+          return page, dataset.limit(per_page, (current - 1) * per_page)
         end
       end
     end
