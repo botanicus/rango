9971f11031d73df115a7d7f905a4e10cc9732e8d
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-23 00:23:12 +0000

    We don't need chainable mixin at all, refactoring

--- lib/rango/mixins/message.rb
+++ lib/rango/mixins/message.rb
@@ -6,41 +6,30 @@ module Rango
     # @since 0.0.2
     # NOTE: it's important to include this mixin after ImplicitRendering mixin
     def self.included(controller)
-      unless controller.method_defined?(:request)
-        raise "Rango::MessageMixin requires method request to be defined"
-      end
       controller.class_eval do
         if self.method_defined?(:context)
-          Rango.logger.debug("Extending context by message")
-          def context
-            super.merge!(message: self.message)
-          end
-        else
-          Rango.logger.warn("Context method isn't defined")
-        end
-
-        def redirect(uri, options = Hash.new, status = 303, &block)
-          # status, options = 303, status if status.is_a?(Hash)
-          if options.respond_to?(:inject)
-            # redirect "/post", error: "Try again"
-            # ?msg[error]="Try again"
-            uri = options.inject(uri) do |uri, pair|
-              type, message = pair
-              uri + "?msg[#{type}]=#{message}"
+          Rango.logger.debug("Extending #{self}#context by message")
+          # This could be in the mixin itself, but we don't want
+          # to define it if the context method doesn't exist, so
+          # context.respond_to?(:context) as a detection of explicit
+          # rendering mixin and similar can work. BTW the following
+          # doesn't work with do/end syntax.
+          include Module.new {
+            def context
+              @context ||= super.merge!(message: self.message)
             end
-          else
-            # redirect "/post", "Try again"
-            # ?msg="Try again"
-            uri.concat("?msg=#{options}")
-          end
-          super(uri, status)
+          }
+        else
+          Rango.logger.warn("Context #{self}#method isn't defined")
         end
       end
     end
 
     def message
+      p [:OR, @message]
       @message ||= begin
-        messages = request.GET[:msg] || String.new
+        messages = request.GET[:msg] || Hash.new
+        p [[messages]]
         if messages.is_a?(String)
           messages.force_encoding(Encoding.default_external)
         elsif messages.is_a?(Hash)
@@ -51,5 +40,22 @@ module Rango
       end
     end
     alias_method :flash, :message
+
+    def redirect(uri, options = Hash.new, status = 303, &block)
+      # status, options = 303, status if status.is_a?(Hash)
+      if options.respond_to?(:inject)
+        # redirect "/post", error: "Try again"
+        # ?msg[error]="Try again"
+        uri = options.inject(uri) do |uri, pair|
+          type, message = pair
+          uri + "?msg[#{type}]=#{message}"
+        end
+      else
+        # redirect "/post", "Try again"
+        # ?msg="Try again"
+        uri.concat("?msg=#{options}")
+      end
+      super(uri, status)
+    end
   end
 end
--- lib/rango/mixins/rack-flash.rb
+++ lib/rango/mixins/rack-flash.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-require "rango/mixins/chainable"
-
 # @see http://nakajima.github.com/rack-flash
 # @example
 #   use Rack::Flash
@@ -16,24 +14,14 @@ module Rango
     # @since 0.2.4
     # NOTE: it's important to include this mixin after ImplicitRendering mixin
     def self.included(controller)
-      unless controller.method_defined?(:request)
-        raise "Rango::MessageMixin requires method request to be defined"
-      end
       if controller.method_defined?(:context)
         Rango.logger.debug("Extending context by message")
         controller.class_eval do
-          extend Chainable
-          chainable do
+          include Module.new {
             def context
-              super.merge!(message: self.message)
+              @context ||= super.merge!(message: self.message)
             end
-
-            def redirect(uri, status = 303, options = Hash.new, &block)
-              status, options = 303, status if status.is_a?(Hash)
-              options.each { |type, message| flash[type] = message }
-              super(uri, status)
-            end
-          end
+          }
         end
       else
         Rango.logger.warn("Context method isn't defined")
@@ -46,5 +34,11 @@ module Rango
       end
     end
     alias_method :flash, :message
+
+    def redirect(uri, status = 303, options = Hash.new, &block)
+      status, options = 303, status if status.is_a?(Hash)
+      options.each { |type, message| flash[type] = message }
+      super(uri, status)
+    end
   end
 end
