56418dc06580d9c0d8f08822a1c96b79389ae66a
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-23 01:45:17 +0000

    Ensure that params have indifferent access

--- spec/rango/controller_spec.rb
+++ spec/rango/controller_spec.rb
@@ -23,4 +23,16 @@ describe Rango::Controller do
     @controller.should respond_to(:logger)
     #@controller.logger.should eql(Project.logger)
   end
+
+  describe "#params" do
+    before(:each) do
+      env = Rack::MockRequest.env_for("/?ssl=true")
+      @controller = Rango::Controller.new(env)
+    end
+
+    it "should have indifferent access" do
+      @controller.params[:ssl].should  eql("true")
+      @controller.params["ssl"].should eql("true")
+    end
+  end
 end
--- spec/rango/rack/request_spec.rb
+++ spec/rango/rack/request_spec.rb
@@ -4,4 +4,15 @@ require_relative "../../spec_helper"
 require "rango/rack/request"
 
 describe Rango::Request do
+  describe "#params" do
+    before(:each) do
+      env = Rack::MockRequest.env_for("/?ssl=true")
+      @request = Rango::Request.new(env)
+    end
+
+    it "should have indifferent access" do
+      @request.params[:ssl].should  eql("true")
+      @request.params["ssl"].should eql("true")
+    end
+  end
 end
